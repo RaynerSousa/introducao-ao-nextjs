a2eb713076ccd212fb9eeb2bca6c0cff37c4a72a
Rayner <raynerwhanyfek@gmail.com>
2025-09-08 11:45:57 -0300

    Mini Calculadora

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import Wellcome from "./components/Wellcome";
-import MiniDisplay from "./components/Display";
+import MiniDisplay from "./components/MineDisplay";
 import LikeButton from "./components/LikeButton";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-180 bg-blue-400">
+    <div className="flex justify-center items-center h-180 bg-blue-900 ">
       <div>
         {/* <Wellcome /> */}
         {/* <MiniDisplay/> */}
-        <LikeButton />
+        {/* <LikeButton /> */}
+        <MiniDisplay/>
       </div>
     </div>
   );
